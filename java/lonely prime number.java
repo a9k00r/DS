835e0f1806560af2140db174f7c3a0dbdfb33d35
ankur <ankurtripathi@airasia.com>
2021-06-13 19:11:35 +0530

    Refactor: lonely prime number

--- java/lonely prime number.java
+++ java/lonely prime number.java
@@ -1,58 +1,43 @@
-
 import java.util.*;
 public class Solution{
 
-    static boolean isPrime(int n)
-    {
+    static boolean isPrime(int n) {
  
-        // Check if number is less than
-        // equal to 1
-        if (n <= 1)
+        if (n <= 1 || n % 2 == 0)
             return false;
  
-        // Check if number is 2
         else if (n == 2)
             return true;
- 
-        // Check if n is a multiple of 2
-        else if (n % 2 == 0)
-            return false;
- 
-        // If not, then just check the odds
+            
         for (int i = 3; i <= Math.sqrt(n); i += 2)
-        {
             if (n % i == 0)
                 return false;
-        }
+                
         return true;
     }
-     
-    public static void main(String []args){
-        
-        String inp = "12, 3, 4, 5, 5, 4, 3, 2, 2, 21, 23, 40, 7, 6, 4, 7";
-        
-        String [] arr = inp.split(", ");
-        
- 
+    
+    public static void getLonelyPrimeNumber(String [] arr) {
         Map <Integer, Integer> map = new HashMap<Integer, Integer>();
         
         for (int i =0; i< arr.length; i++) {
             
             int num = Integer.parseInt(arr[i]);
             
-            if (isPrime(num)) {
-                if (map.containsKey(num)) {
-                    map.put(num, map.get(num)+1) ;
-                }
-                else {
-                    map.put(num, 1);
-                }
-            }
+            if (isPrime(num))
+                map.put(num, map.getOrDefault(num, 0)+1);
         }
         
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> e : map.entrySet())
                if (e.getValue() == 1)
                 System.out.println(e.getKey());
-            }
-     }
+    }
+     
+    public static void main(String [] args) {
+        
+        String inp = "12, 3, 4, 5, 5, 4, 3, 2, 2, 21, 23, 40, 7, 6, 4, 7";
+        
+        String [] arr = inp.split(", ");
+
+        getLonelyPrimeNumber(arr);
+    }
 }
