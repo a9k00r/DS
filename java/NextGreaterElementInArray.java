4d1028e1767d0759bbb48daa908844e358d672ce
ankur <ankurtripathi@airasia.com>
2021-06-13 18:37:17 +0530

    next greater element in 2 arrays

---
+++ java/NextGreaterElementInArray.java
@@ -0,0 +1,44 @@
+/**
+ * Problem:
+496. Next Greater Element I
+
+You are given two integer arrays nums1 and nums2 both of unique elements, where nums1
+is a subset of nums2.Find all the next greater numbers for nums1's elements in the
+corresponding places of nums2.The Next Greater Number of a number x in nums1 is the
+first greater number to its right in nums2. If it does not exist, return -1 for this
+number.
+
+Example 1:
+
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+
+Explanation:
+For number 4 in the first array, you cannot find the next greater number for it in thesecond array, so output -1.
+For number 1 in the first array, the next greater number for it in the second array is 3.
+For number 2 in the first array, there is no next greater number for it in the second array, so output -1.*/
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        int [] res = new int[nums1.length];
+        
+        Map<Integer, Integer> map = new HashMap();
+        Stack<Integer> s = new Stack<Integer>();
+        
+        for (int i = nums2.length -1; i>=0; i--) {
+            while (!s.empty() && s.peek() < nums2[i])
+                s.pop();
+
+            int nextG = s.empty() ? -1 : s.peek();
+            
+            map.put(nums2[i], nextG);
+            s.push(nums2[i]);
+        }
+        
+        for (int i = 0; i< nums1.length; i++)
+            res[i] = map.get(nums1[i]);
+
+        return res;
+    }
+}
