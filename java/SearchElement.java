04a111a53c65caa16e8e1faca5109c80d9343891
ankur <ankurtripathi@airasia.com>
2021-07-08 11:43:06 +0530

    search targer element in 2d matrix

---
+++ java/SearchElement.java
@@ -0,0 +1,41 @@
+/**
+Write an efficient algorithm that searches for a target value in an m x n integer
+matrix. The matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],
+[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],
+[18,21,23,26,30]], target = 20
+Output: false
+*/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int i = 0, j = matrix[0].length - 1;
+        
+        while (i < matrix.length && j >= 0) {
+            
+            if (matrix[i][j] == target) 
+                return true;
+            
+            if (matrix[i][j] > target)
+                j--;
+            else
+                i++;
+                
+        }
+        return false;
+    }
+}
